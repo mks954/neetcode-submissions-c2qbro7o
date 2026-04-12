63b15f118e0f79cc3347b564471e11fc5c653625
mks954 <mks.manik95@gmail.com>
2026-04-12 14:13:03 +0530

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        double[][] time = new double[n][2];
+        for(int i = 0; i < n ; i++){
+            time[i][0] = position[i];
+            time[i][1] = (double) (target-position[i])/speed[i];
+        }
+        Arrays.sort(time, (a,b) -> Double.compare(b[0], a[0]));
+        int ans = 1;
+        double start = time[0][1];
+        for(int i = 1 ; i < n ; i++){
+            if(time[i][1]>start){
+                ans++;
+                start = time[i][1];
+            }
+        }
+        return ans;
+    }
+}
